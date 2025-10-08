0d8801335ed5ff1a8c9c417ef5734850a09611bf
Mike <196670706+theSpacePope91@users.noreply.github.com>
2025-10-08 15:58:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 });
 
 function showPassword() {
-  var x = document.getElementById("password");
+  let x = document.getElementById("password");
   if (x.type === "password") {
     x.type = "text";
   } else {
     x.type = "password";
-    }
+  }
 }

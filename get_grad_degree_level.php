d10d63e0f10f340845bda6f106b160ce9eb2a9b9
Mike Covelli <196670706+theSpacePope91@users.noreply.github.com>
2025-11-26 23:10:40 -0500

    fixed create program. Removed Program ID (auto increment), Program code is derived from DegreeLevel + ProgramName. Fetch DegreeLevel types dynamically from DB

--- get_grad_degree_level.php
+++ get_grad_degree_level.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . '/config.php';
+header('Content-Type: application/json');
+
 $mysqli = get_db();
+$mysqli->set_charset('utf8mb4');
 
 $sql = "SELECT DISTINCT DegreeLevel FROM Program ORDER BY DegreeLevel";
 $result = $mysqli->query($sql);
@@ -10,9 +13,8 @@ $degreeLevel = [];
 while ($row = $result->fetch_assoc()) {
     $degreeLevel[] = [
         'degreelevel' => $row['DegreeLevel']
-    ]
+    ];
 }
 
-header('Content-Type: application/json');
 echo json_encode($degreeLevel);
 ?>

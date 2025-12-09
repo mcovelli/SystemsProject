06da7d3860313406567c266a1c2d5190313a5e11
sunquamkid <jacksonbeach39@gmail.com>
2025-12-09 16:16:17 -0500

    Edited

--- AttendanceHistory.php
+++ AttendanceHistory.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/config.php';
 
 if (
     !isset($_SESSION['user_id']) || (
-        ($_SESSION['role'] ?? '') !== 'student'
+        ($_SESSION['role'] ?? '') !== 'student' &&
         ($_SESSION['role'] ?? '') !== 'faculty' &&
             ($_SESSION['role'] ?? '') !== 'admin')
     )

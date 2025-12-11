fa5955164d06722fbe9dec7394657bafccd9f1b9
Mike Covelli <196670706+theSpacePope91@users.noreply.github.com>
2025-12-10 20:38:28 -0500

    added place hold
    changes

--- createDirectory.php
+++ createDirectory.php
@@ -89,8 +89,11 @@ $otherLinks = [
     ['label' => 'Assign Advisor', 'href' => 'assign_advisor.php',       'icon' => 'file-text'],
     ['label' => 'Declare Major',      'href' => 'DeclareMajor.php',           'icon' => 'book'],
     ['label' => 'Declare Minor',   'href' => 'DeclareMinor.php',                      'icon' => 'book-open'],
-    ['label' => 'Declare Program',     'href' => 'DeclareProgram.php',                      'icon' => 'mail'],
-    ['label' => 'Place Hold',     'href' => 'PlaceHold.php',                      'icon' => 'check']
+    ['label' => 'Declare Program',     'href' => 'DeclareProgram.php',                      'icon' => 'open-book'],
+    ['label' => 'Place Hold',     'href' => 'PlaceHold.php',                      'icon' => 'X']
+
+
+>>>>>>> 459e53e (added place hold)
 ];
 
 $initials = substr($admin['FirstName'], 0, 1) . substr($admin['LastName'], 0, 1);
